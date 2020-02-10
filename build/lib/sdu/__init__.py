880276dc9be078f38e9925ffc5b32133dd0ed327
Kieran Wood <kieranw098@gmail.com>
2020-02-09 21:49:44 -0700

    Updated documentation

---
+++ build/lib/sdu/__init__.py
@@ -0,0 +1,14 @@
+"""A set of utilites to make developing scripts simpler and easier
+
+Modules
+-------
+paths:
+    This module contains many useful utilities for dealing with system paths.
+
+terminal:
+    A module for helpful utilities with generating CLI's
+
+validation:
+    Contains a set of common validation schemes.
+
+"""
